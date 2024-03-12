bca6de68b44c7e38e3a2df8e451f0ce141f63c63
Quentin GODEAU <quentingodeau@users.noreply.github.com>
2024-03-12 23:04:13 +0100

    Fix file upload to azure

--- scripts/upload_test_files_to_azure.sh
+++ scripts/upload_test_files_to_azure.sh
@@ -4,14 +4,14 @@
 # CI can then run tests
 
 # Create container
-az storage container create -n testing-private --account-name $AZURE_STORAGE_ACCOUNT
-az storage container create -n testing-public  --account-name $AZURE_STORAGE_ACCOUNT --public-access container
+az storage fs create --name testing-private --account-name $AZURE_STORAGE_ACCOUNT
+az storage fs create --name testing-public  --account-name $AZURE_STORAGE_ACCOUNT --public-access filesystem
 
 copy_file() {
   local from="${1}"
   local to="${2}"
-  az storage blob upload --file "${from}" --name "${to}" --container-name "testing-private" --account-name "${AZURE_STORAGE_ACCOUNT}" --overwrite
-  az storage blob upload --file "${from}" --name "${to}" --container-name "testing-public"  --account-name "${AZURE_STORAGE_ACCOUNT}" --overwrite
+  az storage fs file upload --source "${from}" --path "${to}" --file-system "testing-private" --account-name "${AZURE_STORAGE_ACCOUNT}" --overwrite
+  az storage fs file upload --source "${from}" --path "${to}" --file-system "testing-public"  --account-name "${AZURE_STORAGE_ACCOUNT}" --overwrite
 }
 
 while read filepath; do
